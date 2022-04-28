c7f0541765ecd58cf856d4885916fe6facc89637
Jennifer Carreno <jennifercarreno72@gmail.com>
2022-04-28 05:01:17 -0700

    users can only add to playlists they've made

--- controllers/playlists.js
+++ controllers/playlists.js
@@ -1,5 +1,4 @@
 const Playlist = require('../models/playlist');
-
 var request = require('request'); // "Request" library
 var Handlebars = require('Handlebars');
 const user = require('../models/user');
@@ -24,7 +23,6 @@ module.exports = (app) => {
       //new playlist form
       app.get('/playlist/new', (req, res) => {
         const currentUser = req.user;
-
         res.render('playlist-new', {currentUser});
        });
 
@@ -58,9 +56,7 @@ module.exports = (app) => {
         try {
             const currentUser = req.user;
             const playlists = await Playlist.find({'created_by': currentUser}).lean()
-            console.log(playlists)
             const track = req.body.track_id;
-            console.log(track)
             return res.render('choose-playlist', {track, playlists, currentUser} )
         } catch (err) {
             console.log(err.message);
@@ -72,10 +68,8 @@ module.exports = (app) => {
     app.post('/playlist/add', async (req, res) => {
         
         try {
-          console.log('PLAYLIST: '+ req.body.playlist_id)
 
-
-            // GETS WHICH PLAYLIST WE SHOULD ADD TOO
+            // GETS WHICH PLAYLIST WE SHOULD ADD TO
             const playlist = await Playlist.findById({_id : req.body.playlist_id}).lean();
 
             // GETS WHICH TRACK WE WANTED TO ADD
@@ -117,63 +111,34 @@ module.exports = (app) => {
                 }
     });
 
-    app.post('/playlist/add/:id', async (req, res) => {
-        
-      try {
-
-          // GETS WHICH PLAYLIST WE SHOULD ADD TOO
-          const playlist = await Playlist.findById(req.params.id).lean().populate('created_by');
-
-          // GETS WHICH TRACK WE WANTED TO ADD
-          let track = req.body.track_id;
-          console.log('TRACK'+ track)
-
-          request.post(authOptions, function(error, response, body){
-
-              if (!error && response.statusCode === 200){
-                var token = body.access_token;
-                var options = {
-                url: `https://api.spotify.com/v1/tracks/${track}`,
-                headers: {
-                    'Authorization': 'Bearer ' + token
-                },
-                json: true
-                }
-              }
-          
-              request.get(options, function(error, response, body) {
-                  // gets track object
-                  track = body
-
-                  // adds track to playlist
-                  Playlist.findOneAndUpdate({_id : playlist._id}, {$push: {tracks:track}}).then((result) => {
-
-                      // returns updated playlist
-                      return res.redirect(`/playlist/${playlist._id}`)
-
-                    }).catch ((err) => {
-                      console.log(err.message);
-                  });
-                
-              });
-          
-            });
-
-          } catch (err) {
-              console.log(err.message);
-              }
-  });
-
 
     // displays one playlist
     app.get('/playlist/:id', async (req, res) => {
         try {
+          if (req.user) {
+            const currentUser = req.user;
+            let author;
+            const search = req.body.search;
+            const playlist = await Playlist.findById(req.params.id).lean().populate('created_by');
+
+            if(currentUser.username == playlist.created_by.username){
+              author = true;
+              return res.render('playlist-show', {playlist, currentUser, search, author})
+
+            } else {
+              return res.render('playlist-show', {playlist, currentUser, search})
+
+            }
+          } else {
             const currentUser = req.user;
             const search = req.body.search;
             const playlist = await Playlist.findById(req.params.id).lean().populate('created_by');
-            console.log(playlist.tracks);
-            
             return res.render('playlist-show', {playlist, currentUser, search})
+
+
+          }
+            
+            
         } catch(err){
             console.log(err.message);
         }
@@ -183,27 +148,28 @@ module.exports = (app) => {
     // displays all playlists
     app.get('/playlists', async (req, res) =>{
         try {
+          if(req.user){
+            const currentUser = req.user;
+            const playlists = await Playlist.find({'created_by': currentUser}).lean().populate('created_by')
+            return res.render('playlists', {playlists, currentUser})
+
+          } else {
             const currentUser = req.user;
             const playlists = await Playlist.find({}).lean().populate('created_by')
-
-
-            
             return res.render('playlists', {playlists, currentUser})
+          }
+            
         } catch(err) {
-
+            console.log(err.message);
         }
     });
+
 
     app.post('/playlist/search/:id', async(req, res) => {
       try {
         const currentUser = req.user;
         const playlist = await Playlist.findById(req.params.id).lean().populate('created_by');
-
-        console.log('PLAYLIST ID' + playlist.tracks)
-    
-      // console.log(req.body.search);
         let search = req.body.search
-      // console.log(search)
     
       request.post(authOptions, function(error, response, body){
     
@@ -219,23 +185,13 @@ module.exports = (app) => {
         }
     
         request.get(options, function(error, response, body) {
-          console.log('PLAYLIST TO BE ADDED:' + playlist._id)
-
           const tracks = body.tracks.items
 
           for (i in tracks) {
-            console.log('TRACK NAME: '+ tracks[i].name);
+            // adds playlist attribute to tracks
             tracks[i].playlist = playlist._id;
-          }
-        
-          
-
-          // console.log(body.tracks.items[0].album.images[0].url);
-          
-    
+          }          
           return res.render('playlist-show', {search, tracks, playlist, currentUser})
-    
-          
         });
         
       });
@@ -243,6 +199,8 @@ module.exports = (app) => {
       console.log(err.message);
     }
     });
+
+
 
 
 };
