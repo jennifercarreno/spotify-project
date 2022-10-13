24ad9dfb1e0f0c826574a7d4c0589fd41357461d
Jennifer Carreno <jennifercarreno72@gmail.com>
2022-10-13 01:03:03 -0700

    rough outline for publishing playlists, one working route publishing test playlist

--- controllers/spotify-test.js
+++ controllers/spotify-test.js
@@ -18,25 +18,39 @@ var authOptions = {
 
 module.exports = (app) => {
 
+    //very basic very bad but working way of publishing a playlist 
+
     app.get('/spotifytest', (req, res) => {
 
         request.post(authOptions, function(error, response, body) {
             if (!error && response.statusCode === 200) {
           
               // use the access token to access the Spotify Web API
-              var token = body.access_token;
+              var token = 'BQCquyBOfiAXlS-npLkrAi3Gs8yEYGqubkKGK-joZzEVu2s89s5s7Y3tw71z4LdK14AnhTDENQgEh4fJwzYbSkvvJDrzhWSDsAZBDNitZQZtNYJOQ5LbLymplWQHLtEpGv6b-8yy4oXahwo88vR6tD67zCOzC9gIePXreFGp4_RCblzsrnUduTGhOaR26BJTrlgpz2Q7nVPlekLHTUmub2DWWJLZobVT0pELBCMSHEaXe6ANF1kHkp1n5YPVAK9y';
               var options = {
-                url: 'https://api.spotify.com/v1/users/313oodjlqlgtvp6joqiomhfumlyq',
+                url: 'https://api.spotify.com/v1/users/313oodjlqlgtvp6joqiomhfumlyq/playlists',
+                
                 headers: {
-                  'Authorization': 'Bearer ' + token
+                  'Authorization': 'Bearer ' + token,
+                  'Content-Type': 'application/json'
                 },
-                json: true
+
+                body: JSON.stringify({
+                    'name': 'Test Playlist',
+                    'public': true
+                }),
+                dataType:'json',
+                
+                
               };
-              request.get(options, function(error, response, body) {
+
+              request.post(options, function(error, response, body) {
                 console.log(body);
               });
             }
           });
+          
+        
     })
     
 
